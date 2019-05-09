cd10e8497f34b54f957f53197f86a6135e099ad7
lukaszg84 <lugr+git@google.com>
2019-05-09 11:49:34 +0000

    USe rate limited client in spotify

--- spotify/spotify.go
+++ spotify/spotify.go
@@ -3,26 +3,35 @@ package spotify
 import (
 	"context"
 	"ojoj.ch/oauth"
+	"ojoj.ch/ratelimit"
+	"time"
 )
 
 type Spotify struct {
-	oauthClient *oauth.OAuth
+	// Client with 1 qps limit
+	httpClient ratelimit.AnyClient
 }
 
-func New(ctx context.Context) *Spotify {
+func New(ctx context.Context) (*Spotify, error) {
 	// First create OAuth.
 	oauthClient, err := oauth.Create("spotify")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// Verify the token
 	err = oauthClient.VerifyToken(ctx)
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+
+	// Get http client with Bearer
+	httpClient, err := oauthClient.CreateAuthenticatedHttpClient(ctx)
+	if err != nil {
+		return nil, err
 	}
 
 	return &Spotify{
-		oauthClient: oauthClient,
-	}
+		httpClient: ratelimit.New(httpClient, time.Second),
+	}, nil
 }
